8eb770f549e40fc8c7d5d0a747b03aee16cc931e
alanocb <99679262+alanocb@users.noreply.github.com>
2023-01-14 15:58:24 +0000

    Delete getmatch directory

--- getmatch/getmatch/getmatch/getmatch/src/main/java/pt/iade/getmatch/models/Repository/ModalidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.iade.getmatch.models.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pt.iade.getmatch.models.Modalidade;
-
-public interface ModalidadeRepository extends JpaRepository<Modalidade, Integer>{
-    
-}
